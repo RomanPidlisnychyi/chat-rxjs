7aea50aad4323f15c84b273210826b318a3bb880
RomanPidlisnychyi <myluckyhelper@gmail.com>
2022-09-02 10:21:27 +0300

    testing

--- src/App.js
+++ src/App.js
@@ -4,6 +4,7 @@ import Switcher from './components/Switcher';
 import Test from './components/Test';
 
 function App() {
+  // !TODO checking branch rename
   return (
     <div className="App">
       <Switcher />
